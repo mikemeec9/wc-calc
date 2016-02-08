818ef6168493131410108a00c1bb92302a69a58f
mm2001 <mikemeec9@mikemee.com>
2016-02-08 18:50:55 +0000

    Current site snapshot

---
+++ tests.js
@@ -0,0 +1,211 @@
+var assert = require('assert'),
+    wpcalc = require('./wpcalc');
+
+module.exports = {
+    'exists()': function() {
+        assert.equal(true, wpcalc.exists());
+    },
+
+    'baseFee_lt3M': function() {
+        assert.equal(1000, wpcalc.baseFee(1000));
+        assert.equal(1000, wpcalc.baseFee(100000));
+        assert.equal(1000, wpcalc.baseFee(1000000));
+        assert.equal(1000, wpcalc.baseFee(2000000));
+        assert.equal(1000, wpcalc.baseFee(3000000));
+        assert.equal(1000, wpcalc.baseFee("3000000"));
+        assert.notEqual(1000, wpcalc.baseFee(3000001));
+    },
+
+    'baseFee_bw36M': function() {
+        assert.notEqual(4000, wpcalc.baseFee(3000000));
+        assert.equal(4000, wpcalc.baseFee(3000001));
+        assert.equal(4000, wpcalc.baseFee(5000000));
+        assert.equal(4000, wpcalc.baseFee(6000000));
+        assert.equal(4000, wpcalc.baseFee("6000000"));
+        assert.notEqual(4000, wpcalc.baseFee(6000001));
+    },
+
+    'baseFee_gt6M': function() {
+        assert.notEqual(4000, wpcalc.baseFee(6000001));
+        assert.equal(5000, wpcalc.baseFee(6000001));
+        assert.equal(5000, wpcalc.baseFee(10000000));
+    },
+
+    'baseFee_NaN': function() {
+        assert.ok(isNaN(wpcalc.baseFee(-1)));
+        assert.ok(isNaN(wpcalc.baseFee('asdf')));
+        assert.ok(isNaN(wpcalc.baseFee(0)));
+    },
+
+    'basisPoints_lt500k': function () {
+        assert.equal(0, wpcalc.basisPoints(1));
+        assert.equal(0, wpcalc.basisPoints(1000));
+        assert.equal(0, wpcalc.basisPoints(500000));
+        assert.equal(0, wpcalc.basisPoints("500000"));
+        assert.notEqual(0, wpcalc.basisPoints(500001));
+        assert.notEqual(0, wpcalc.basisPoints(1000000));
+    },
+
+    'basisPoints_bw53M': function() {
+        assert.equal(0.001, wpcalc.basisPoints(500001));
+        assert.equal(0.5, wpcalc.basisPoints(500500));
+        assert.equal(500, wpcalc.basisPoints(1000000));
+        assert.equal(1500, wpcalc.basisPoints(2000000));
+        assert.equal(2499.999, wpcalc.basisPoints(2999999).toFixed(3));
+        assert.equal(2500, Math.round(wpcalc.basisPoints(2999999)));
+        assert.equal(779, Math.round(wpcalc.basisPoints(1278867)));
+        assert.equal(0, wpcalc.basisPoints(3000000));
+
+    },
+
+    'basisPoints_NaN': function () {
+        assert.ok(isNaN(wpcalc.basisPoints(-1)));
+        assert.ok(isNaN(wpcalc.basisPoints('asdf')));
+        assert.ok(isNaN(wpcalc.basisPoints(0)));
+    },
+
+    'baseFee': function() {
+        assert.equal(1000, wpcalc.baseFee(wpcalc.totalWithAnnual(1278867, 1, 133779)));
+        assert.equal(1000, wpcalc.baseFee(wpcalc.totalWithAnnual(1278867, 2, 133779)));
+        assert.equal(1000, wpcalc.baseFee(wpcalc.totalWithAnnual(1278867, 3, 133779)));
+        assert.equal(1000, wpcalc.baseFee(wpcalc.totalWithAnnual(1278867, 4, 133779)));
+        assert.equal(1000, wpcalc.baseFee(wpcalc.totalWithAnnual(1278867, 5, 133779)));
+    },
+
+    'basisPoints': function() {
+        assert.equal(779, Math.round(wpcalc.basisPoints(wpcalc.totalWithAnnual(1278867, 1, 133779))));
+        assert.equal(913, Math.round(wpcalc.basisPoints(wpcalc.totalWithAnnual(1278867, 2, 133779))));
+        assert.equal(1046, Math.round(wpcalc.basisPoints(wpcalc.totalWithAnnual(1278867, 3, 133779))));
+        assert.equal(1180, Math.round(wpcalc.basisPoints(wpcalc.totalWithAnnual(1278867, 4, 133779))));
+        assert.equal(1314, Math.round(wpcalc.basisPoints(wpcalc.totalWithAnnual(1278867, 5, 133779))));
+    },
+
+    'recordKeeping': function() {
+        assert.equal(0, wpcalc.recordKeeping(1));
+        assert.equal(0, wpcalc.recordKeeping(2));
+        assert.equal(0, wpcalc.recordKeeping(15));
+        assert.equal(75, wpcalc.recordKeeping(16));
+        assert.equal(150, wpcalc.recordKeeping(17));
+        assert.equal(375, wpcalc.recordKeeping(20));
+        assert.equal(2625, wpcalc.recordKeeping(50));
+        assert.equal(2695, wpcalc.recordKeeping(51));
+        assert.equal(6125, wpcalc.recordKeeping(100));
+        assert.equal(6190, wpcalc.recordKeeping(101));
+        assert.equal(12625, wpcalc.recordKeeping(200));
+        assert.equal(153350, wpcalc.recordKeeping(2365));
+    },
+
+    'recordKeeping_NaN': function() {
+        assert.ok(isNaN(wpcalc.recordKeeping(-1)));
+        assert.ok(isNaN(wpcalc.recordKeeping('asdf')));
+        assert.ok(isNaN(wpcalc.recordKeeping(0)));
+    },
+
+    'recordKeepingCredit': function() {
+        assert.equal(-10, wpcalc.recordKeepingCredit(wpcalc.totalWithAnnual(10000, 1, 0)));
+        assert.equal(-1278.87, wpcalc.recordKeepingCredit(wpcalc.totalWithAnnual(1278867, 1, 133779)).toFixed(2));
+    },
+
+    'recordKeepingCredit_NaN': function() {
+        assert.ok(isNaN(wpcalc.recordKeepingCredit(-1)));
+        assert.ok(isNaN(wpcalc.recordKeepingCredit('asdf')));
+        assert.ok(isNaN(wpcalc.recordKeepingCredit(0)));
+    },
+
+    'totalWithAnnual': function() {
+        assert.equal(1, wpcalc.totalWithAnnual(1, 1, 0));
+        assert.equal(1, wpcalc.totalWithAnnual("1", "1", 0));
+        assert.equal(1, wpcalc.totalWithAnnual(1, 1, 1));
+        assert.equal(2, wpcalc.totalWithAnnual(1, 2, 1));
+        assert.equal(2, wpcalc.totalWithAnnual(1, 2, 1));
+        assert.equal(3, wpcalc.totalWithAnnual(1, 3, 1));
+        assert.equal(4, wpcalc.totalWithAnnual(1, 4, 1));
+        assert.equal(5, wpcalc.totalWithAnnual(1, 5, 1));
+
+        assert.equal(5, wpcalc.totalWithAnnual('1', 5, 1));
+        assert.equal(5, wpcalc.totalWithAnnual(1, '5', 1));
+        assert.equal(5, wpcalc.totalWithAnnual(1, '5', '1'));
+    },
+
+    'totalWithAnnual_NaN': function() {
+        assert.ok(isNaN(wpcalc.totalWithAnnual(-1)));
+        assert.ok(isNaN(wpcalc.totalWithAnnual('asdf')));
+        assert.ok(isNaN(wpcalc.totalWithAnnual(1, 'asdf', 0)));
+        assert.ok(isNaN(wpcalc.totalWithAnnual(1, 1, 'asdf')));
+    },
+
+    'fundExpenses': function() {
+        assert.equal(1.9, wpcalc.fundExpenses(1000, 19));
+        assert.equal(12.9, wpcalc.fundExpenses(1000, 129));
+        assert.equal(10, wpcalc.fundExpenses(1000, 100));
+        assert.equal(20, wpcalc.fundExpenses(1000, 200));
+    },
+
+    'fundExpenses_NAN': function() {
+        assert.ok(isNaN(wpcalc.fundExpenses()));
+        assert.ok(isNaN(wpcalc.fundExpenses(1)));
+        assert.ok(isNaN(wpcalc.fundExpenses('asdf', 1)));
+        assert.ok(isNaN(wpcalc.fundExpenses(1, 'asdf')));
+    },
+
+    'firstYearCosts': function() {
+        assert.equal(6780, Math.round(wpcalc.firstYear(1278867, 20, 133779, 19)));
+        assert.equal(6780, Math.round(wpcalc.firstYear("1278867", "20", "133779", 19)));
+    },
+
+    'firstYearCosts_NaN': function() {
+        assert.ok(isNaN(wpcalc.firstYear()));
+        assert.ok(isNaN(wpcalc.firstYear(1)));
+        assert.ok(isNaN(wpcalc.firstYear(1,2)));
+        assert.ok(isNaN(wpcalc.firstYear(1,2,3)));
+        assert.ok(isNaN(wpcalc.firstYear('asdf',2,3)));
+        assert.ok(isNaN(wpcalc.firstYear(1,'asdf',3)));
+        assert.ok(isNaN(wpcalc.firstYear(1,2,'asdf')));
+    },
+
+    'cumulativeCost': function() {
+        assert.equal(12789, wpcalc.cumulativeCost(1278867, 133779, 1, 100));
+        assert.equal(12789, wpcalc.cumulativeCost('1278867', 133779, 1, 100));
+        assert.equal(12789, wpcalc.cumulativeCost(1278867, '133779', 1, 100));
+        assert.equal(12789, wpcalc.cumulativeCost(1278867, 133779, '1', 100));
+        assert.equal(12789, wpcalc.cumulativeCost(1278867, 133779, 1, '100'));
+
+
+        assert.equal(26915, wpcalc.cumulativeCost(1278867, 133779, 2, 100));
+        assert.equal(42379, wpcalc.cumulativeCost(1278867, 133779, 3, 100));
+        assert.equal(59181, wpcalc.cumulativeCost(1278867, 133779, 4, 100));
+        assert.equal(77321, wpcalc.cumulativeCost(1278867, 133779, 5, 100));
+
+        assert.equal(19183, wpcalc.cumulativeCost(1278867, 133779, 1, 150));
+        assert.equal(40373, wpcalc.cumulativeCost(1278867, 133779, 2, 150));
+        assert.equal(63569, wpcalc.cumulativeCost(1278867, 133779, 3, 150));
+        assert.equal(88772, wpcalc.cumulativeCost(1278867, 133779, 4, 150));
+        assert.equal(115982, wpcalc.cumulativeCost(1278867, 133779, 5, 150));
+
+        assert.equal(25577, wpcalc.cumulativeCost(1278867, 133779, 1, 200));
+        assert.equal(53830, wpcalc.cumulativeCost(1278867, 133779, 2, 200));
+        assert.equal(84759, wpcalc.cumulativeCost(1278867, 133779, 3, 200));
+        assert.equal(118363, wpcalc.cumulativeCost(1278867, 133779, 4, 200));
+        assert.equal(154643, wpcalc.cumulativeCost(1278867, 133779, 5, 200));
+    },
+
+    'cumulativeSavings': function() {
+        assert.equal(37380, wpcalc.cumulativeSavings('1278867', 133779, 20, 100));
+        assert.equal(37380, wpcalc.cumulativeSavings(1278867, '133779', 20, 100));
+        assert.equal(37380, wpcalc.cumulativeSavings(1278867, 133779, '20', 100));
+        assert.equal(37380, wpcalc.cumulativeSavings(1278867, 133779, 20, '100'));
+        assert.equal(37380, wpcalc.cumulativeSavings(1278867, 133779, 20, 100));
+        assert.equal(76041, wpcalc.cumulativeSavings(1278867, 133779, 20, 150));
+        assert.equal(114702, wpcalc.cumulativeSavings(1278867, 133779, 20, 200));
+    },
+
+    'cumulativeSavings_NaN': function() {
+        assert.ok(isNaN(wpcalc.cumulativeSavings(1278867)));
+        assert.ok(isNaN(wpcalc.cumulativeSavings(1278867, 133779)));
+        assert.ok(isNaN(wpcalc.cumulativeSavings(1278867, 133779, 20)));
+        assert.ok(isNaN(wpcalc.cumulativeSavings('asdf', 133779, 20, 100)));
+        assert.ok(isNaN(wpcalc.cumulativeSavings(1278867, 'asdf', 20, 100)));
+        assert.ok(isNaN(wpcalc.cumulativeSavings(1278867, 133779, 'asdf', 100)));
+        assert.ok(isNaN(wpcalc.cumulativeSavings(1278867, 133779, 20, 'asdf')));
+    },
+};
---
+++ wpcalc.js
@@ -0,0 +1,216 @@
+
+/*global define: true, jQuery: false */
+
+if (typeof define !== 'function') {
+    var define = require('amdefine')(module);
+}
+
+define(function (require, exports, module) {
+
+    "use strict";
+
+    exports.exists = function () {
+        return true;
+    };
+
+    exports.baseFee = function (amount) {
+        var fee = NaN,
+            million = 1000000;
+        if (isFinite(String(amount)) && amount > 0) {
+            if (amount <= 3 * million) {
+                fee = 1000;
+            } else if (amount <= 6 * million) {
+                fee = 4000;
+            } else if (amount > 6 * million) {
+                fee = 5000;
+            }
+        }
+        return fee;
+    };
+
+    exports.basisPoints = function (amount) {
+        var points = NaN,
+            million = 1000000;
+        if (isFinite(String(amount)) && amount > 0) {
+            if (amount <= (million / 2)) {
+                points = 0;
+            } else if (amount > (million / 2) && amount < 3 * million) {
+                points = 0.001 * (amount - (million / 2));
+            } else if (amount >= million * 3) {
+                points = 0;
+            }
+        }
+        return points;
+    };
+
+    exports.recordKeeping = function (userCount) {
+        var cost = NaN;
+        if (isFinite(String(userCount)) && userCount > 0) {
+            if (userCount <= 15) {
+                cost = 0;
+            } else if (userCount <= 50) {
+                cost = (userCount - 15) * 75;
+            } else if (userCount <= 100) {
+                cost = 2625 + (userCount - 50) * 70;
+            } else {
+                cost = 6125 + (userCount - 100) * 65;
+            }
+        }
+        return cost;
+    };
+
+    exports.recordKeepingCredit = function (assets) {
+        var credit = NaN;
+        if (isFinite(String(assets)) && assets > 0) {
+            credit = -0.001 * assets;
+        }
+        return credit;
+    };
+
+    exports.totalWithAnnual = function (assets, year, deposit) {
+        var total = NaN;
+        if (isFinite(String(assets)) && assets > 0 &&
+                isFinite(String(year)) && year > 0 &&
+                isFinite(String(deposit)) && deposit >= 0) {
+            total = (year - 1) * deposit + Number(assets);
+        }
+        return total;
+    };
+
+    exports.fundExpenses = function (assets, points) {
+        var total = NaN;
+        if (isFinite(String(assets)) && assets > 0 &&
+                isFinite(String(points)) && points > 0) {
+            total = points * assets / 10000;
+        }
+        return total;
+    };
+
+    exports.firstYear = function (assets, userCount, deposit, points) {
+        var total = NaN, totalAssets;
+        if (isFinite(String(assets)) && assets > 0 &&
+                isFinite(String(userCount)) && userCount > 0 &&
+                isFinite(String(deposit)) && deposit >= 0 &&
+                isFinite(String(points)) && points >= 0) {
+            totalAssets = exports.totalWithAnnual(assets, 1, deposit);
+            total = exports.baseFee(totalAssets) +
+                    exports.basisPoints(totalAssets) +
+                    3475 +
+                    exports.recordKeeping(userCount) +
+                    exports.recordKeepingCredit(totalAssets) +
+                    exports.fundExpenses(totalAssets, points);
+        }
+        return total;
+    };
+
+    exports.cumulativeCost = function (assets, deposit, year, points) {
+        var total = NaN,
+            i;
+        if (isFinite(String(assets)) && assets > 0 &&
+                isFinite(String(deposit)) && deposit >= 0 &&
+                isFinite(String(year)) && year >= 0 &&
+                isFinite(String(points)) && points >= 0) {
+            total = 0;
+            for (i = 0; i < year; i += 1) {
+                total += (points / 10000) * ((i * deposit) + Number(assets));
+            }
+        }
+        return Math.round(total);
+    };
+
+    exports.cumulativeSavings = function (assets, deposit, userCount, points) {
+        var total = NaN,
+            wpPoints = 19,
+            year;
+        if (isFinite(String(assets)) && assets > 0 &&
+                isFinite(String(deposit)) && deposit >= 0 &&
+                isFinite(String(userCount) && userCount > 0) &&
+                isFinite(String(points)) && points >= 0) {
+            total = 3500;
+            for (year = 1; year <= 5; year += 1) {
+                total += exports.firstYear(exports.totalWithAnnual(assets, year, deposit), userCount, 0, wpPoints);
+            }
+        }
+        return Math.round(exports.cumulativeCost(assets, deposit, 5, points) - total);
+    };
+
+    // http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
+    Number.prototype.formatMoney = function (c, d, t) {
+        var n = this, i, j, s;
+        c = isNaN(c = Math.abs(c)) ? 2 : c;
+        d = d === undefined ? "." : d;
+        t = t === undefined ? "," : t;
+        s = n < 0 ? "-" : "";
+        i = String(parseInt(n = Math.abs(+n || 0).toFixed(c), 10));
+        j = (j = i.length) > 3 ? j % 3 : 0;
+        return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+    };
+
+    exports.updatePage = function (assets, userCount, deposits) {
+        var $ = jQuery,
+            f_assets = $("#assets").val().replace(/[$,]+/g, ""),
+            f_participants = parseInt($("#participants").val(), 10),
+            f_deposits = $("#deposits").val().replace(/[$,]+/g, ""),
+            f_planfees = parseFloat($("#planfees").val().replace(/[$,]+/g, "")),
+            f_planfeesp = parseFloat($("#planfeesp").val()),
+            year1cost = Math.round(exports.firstYear(f_assets, f_participants, f_deposits, 19));
+
+        if (!isFinite(String(f_planfees)) && isFinite(String(f_planfeesp))) {
+            f_planfees = f_assets * f_planfeesp / 100;
+        } else if (isFinite(String(f_planfees)) && !isFinite(String(f_planfeesp))) {
+            f_planfeesp = (f_planfees / f_assets * 100).toFixed(2);
+        } else if (isFinite(String(f_planfees)) && isFinite(String(f_planfeesp))) {
+            f_planfeesp = (f_planfees / f_assets * 100).toFixed(2);
+        } else {
+//            f_planfeesp = 2.5;
+//            f_planfees = f_assets * f_planfeesp / 100;
+            f_planfees = f_planfeesp = "";
+        }
+
+        f_planfees = Math.round(f_planfees);
+        $("#planfees").val(f_planfees);
+        $("#planfeesp").val(f_planfeesp);
+
+        $(".year1cost").text(year1cost.formatMoney(0));
+        $(".year1costp").text((100 * year1cost / f_assets).toFixed(2));
+        $(".year1current").text(f_planfees.formatMoney(0));
+        $(".year1currentp").text((100 * f_planfees / f_assets).toFixed(2));
+
+        $(".year1cost100").text(((100 * f_assets) / 10000).formatMoney(0));
+        $(".year1cost150").text(((150 * f_assets) / 10000).formatMoney(0));
+        $(".year1cost200").text(((200 * f_assets) / 10000).formatMoney(0));
+
+
+        $(".year1save").text(exports.cumulativeSavings(f_assets, f_deposits, f_participants, 100 * f_planfeesp).formatMoney(0));
+        $(".year5save100").text(exports.cumulativeSavings(f_assets, f_deposits, f_participants, 100).formatMoney(0));
+        $(".year5save150").text(exports.cumulativeSavings(f_assets, f_deposits, f_participants, 150).formatMoney(0));
+        $(".year5save200").text(exports.cumulativeSavings(f_assets, f_deposits, f_participants, 200).formatMoney(0));
+
+        /*
+        1.  if total current plan fees <= 0.4 then message A
+        2.  0.4<if total current fees<=0.85 then message B
+        3.  0.85<total current fees<=1.2 then message C
+        4.  If Total current fees >1.2 then message D
+        */
+        $("#messageA").hide();
+        $("#messageB").hide();
+        $("#messageC").hide();
+        $("#messageD").hide();
+        $("#messageE").hide();
+
+        if (!isNaN(f_planfeesp)) {
+            if (f_planfeesp <= 0.4) {
+                $("#messageA").show();
+            } else if (f_planfeesp <= 0.85) {
+                $("#messageB").show();
+            } else if (f_planfeesp <= 1.25) {
+                $("#messageC").show();
+            } else if (f_planfeesp > 1.2) {
+                $("#messageD").show();
+            } else {
+                $("#messageE").show();
+                $("#resultstable").hide();
+            }
+        }
+	};
+});
